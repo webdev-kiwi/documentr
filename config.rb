b1594228e19454f1a6d88d69cae836d0a6e58058
Joseph Patton-Robson <joe.patton-robson@umbrellar.com>
2016-06-19 03:16:00 +1200

    beta 1.0

---
+++ config.rb
@@ -0,0 +1,89 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # Enable cache buster
+  activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+
+  # Optimise images
+  activate :imageoptim do |options|
+    # Use a build manifest to prevent re-compressing images between builds
+    options.manifest = true
+
+    # Silence problematic image_optim workers
+    options.skip_missing_workers = true
+
+    # Cause image_optim to be in shouty-mode
+    options.verbose = false
+
+    # Setting these to true or nil will let options determine them (recommended)
+    options.nice = true
+    options.threads = true
+
+    # Image extensions to attempt to compress
+    options.image_extensions = %w(.png .jpg .gif .svg)
+
+    # Compressor worker options, individual optimisers can be disabled by passing
+    # false instead of a hash
+    options.advpng    = { :level => 4 }
+    options.gifsicle  = { :interlace => false }
+    options.jpegoptim = { :strip => ['all'], :max_quality => 100 }
+    options.jpegtran  = { :copy_chunks => false, :progressive => true, :jpegrescan => true }
+    options.optipng   = { :level => 6, :interlace => false }
+    options.pngcrush  = { :chunks => ['all'], :fix => false, :brute => false }
+    options.pngout    = { :copy_chunks => false, :strategy => 0 }
+    options.svgo      = {}
+  end
+end
